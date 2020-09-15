f2d13fbdc9cc964368406884c24c168ce612c302
Caio Albuquerque <kaka_victor@hotmail.com>
2020-09-15 17:28:14 -0300

    more some a few start things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UtilService } from './services/util.service';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lane-lacos';
   isOpenCloseNav = false;
+  isFixedOpenCloseNav = false;
   myContainer = document.getElementById('mySidebar') as HTMLInputElement;
 
-  toggleSideBarr(){
-    console.log("BVAS");
-    // ('#sidebar').toggleClass('active');
-    this.myContainer.toggleAttribute('active');
-  }
+  constructor(
+    public util: UtilService
+  ) { }
+
   /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
   openNav() {
     document.getElementById('mySidebar').style.width = "250px";
@@ -26,14 +27,23 @@ export class AppComponent {
     document.getElementById('mySidebar').style.width = "50px";
     document.getElementById('main').style.marginLeft = "0";
   }
-  openCloseNav() {
-    this.isOpenCloseNav = !this.isOpenCloseNav;
-    if (this.isOpenCloseNav) {
-      document.getElementById('mySidebar').style.width = "250px";
-      document.getElementById('main').style.marginLeft = "250px";
-    } else {
-      document.getElementById('mySidebar').style.width = "50px";
-      document.getElementById('main').style.marginLeft = "0";
+  fixedSideBar() {
+    this.isFixedOpenCloseNav = !this.isFixedOpenCloseNav;
+  }
+  openCloseNav(param) {
+    // this.isFixedOpenCloseNav =  param === 'fixed' ? true : false;
+    // if (param === 'fixed') {
+    //   this.isFixedOpenCloseNav = !this.isFixedOpenCloseNav;
+    // }
+    if (!this.isFixedOpenCloseNav) {
+      this.isOpenCloseNav = !this.isOpenCloseNav;
+      if (this.isOpenCloseNav) {
+        document.getElementById('mySidebar').style.left = "0px"
+        document.getElementById('main').style.marginLeft = "230px";
+      } else {
+        document.getElementById('mySidebar').style.left = "-200px";
+        document.getElementById('main').style.marginLeft = "30px";
+      }
     }
   }
 }
